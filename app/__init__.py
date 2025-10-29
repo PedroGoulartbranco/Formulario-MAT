e1fb5eddc646135b097cda6a4cbca831e4e4c2e2
André Luiz Holler <andreluizholler@gmail.com>
2025-10-29 08:45:09 -0300

    fiz no chrome

--- app/__init__.py
+++ app/__init__.py
@@ -3,23 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-database_url = os.environ.get('DATABASE_URL')
+def create_app():
+    app = Flask(__name__)
+    
+    
+    database_url = os.environ.get('DATABASE_URL')
+    
+    # Render às vezes fornece a URL com "postgres://" (antigo),
+    # então substituímos por "postgresql://" para compatibilidade com SQLAlchemy
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
+    # Define a URI do banco
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'hfjsdsdksk!!dfhdksfhdjdhjks'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from app.views import pagina_formulario
 
-# Render às vezes fornece a URL com "postgres://" (antigo),
-# então substituímos por "postgresql://" para compatibilidade com SQLAlchemy
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    with app.app_context():
+        db.create_all()
 
-# Define a URI do banco
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'hfjsdsdksk!!dfhdksfhdjdhjks'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-db.create_all()
-
-from app.views import pagina_formulario
+    return app
